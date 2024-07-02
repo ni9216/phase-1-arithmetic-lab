0d0684a8af2ff9c6b914a03f14ef7b06e5c6e3cb
liech <nicholasliech851@gmail.com>
2024-07-02 14:19:08 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+// declare variable
+let num1 = 31;
+let num2 = 2;
+   let multiply = num1 * num2;
+
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1, 5, 10, 15,20);
+console.log(max);
+
